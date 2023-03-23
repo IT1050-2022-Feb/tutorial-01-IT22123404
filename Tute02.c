efcc693b97bbfc1ece334f89d30f7fcd7b0c9a4d
IT22123404 <127759884+IT22123404@users.noreply.github.com>
2023-03-23 02:39:47 +0000

    Tute02

--- Tute02.c
+++ Tute02.c
@@ -16,9 +16,23 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
-
-int main() {
-  
-  return 0;
+#include<stdio.h>
+int main()
+{
+	int distance;
+	float payment;
+	printf("Enter Distance: ");
+	scanf("%d",&distance);
+	
+	if(distance<=30)
+	{
+		payment=distance*50.0;
+	}
+	else
+	{
+		payment=30.0*50+(distance-30)*40;	
+	}
+	printf("Payment=%.2f",payment);
+	
+	return 0;
 }
